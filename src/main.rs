f48586c71c48cb11017e7c3628776ca7f851954f
Chanon <chanon.chanpiwat@gmail.com>
2024-08-18 14:51:01 +0700

    feat: implement threadpool

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use std::{thread, time::{Duration, Instant}};
+use web_server::threadpool::ThreadPool;
+
+
 fn main() {
-    println!("Hello, world!");
+    let start = Instant::now();
+
+    let cores = std::thread::available_parallelism().unwrap();
+    
+    let pools = ThreadPool::new(cores.into());
+
+    let sleep = || {
+        println!("Sleep 5 s");
+        thread::sleep(Duration::from_secs(5));
+        println!("Sleep finished");
+    };
+
+    for _ in 0..28 {
+        pools.execute(sleep);
+    }
+
+
+    drop(pools);
+
+    println!("time taken :{:?}", start.elapsed())
 }
---
+++ src/threadpool/mod.rs
@@ -0,0 +1,83 @@
+use std::{
+    sync::{
+        mpsc::{self, Receiver, Sender},
+        Arc, Mutex,
+    },
+    thread::{self, JoinHandle},
+};
+
+type Job = Box<dyn FnOnce() + Send + 'static>;
+
+struct Worker {
+    _uid: usize,
+    thread: Option<JoinHandle<()>>,
+}
+
+impl Worker {
+    fn new(id: usize, receiver: Arc<Mutex<Receiver<Job>>>) -> Worker {
+        let builder = thread::Builder::new();
+
+        let thread = builder
+            .spawn(move || loop {
+                let message = receiver.lock().unwrap().recv();
+                match message {
+                    Ok(job) => {
+                        job();
+                    },
+                    Err(_) => {
+                        break;
+                    },
+                }
+
+            })
+            .expect("pool size should not exceed limit");
+
+        Worker {
+            _uid: id,
+            thread: Some(thread),
+        }
+    }
+}
+
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+    sender: Option<Sender<Job>>,
+}
+
+impl ThreadPool {
+    pub fn new(size: usize) -> ThreadPool {
+        assert!(size > 0);
+
+        let (sender, receiver) = mpsc::channel();
+        let receiver = Arc::new(Mutex::new(receiver));
+
+        let mut workers = Vec::with_capacity(size);
+
+        for id in 0..size {
+            workers.push(Worker::new(id, Arc::clone(&receiver)))
+        }
+
+        ThreadPool { workers, sender: Some(sender) }
+    }
+}
+
+impl ThreadPool {
+    pub fn execute<F>(&self, f: F)
+    where
+        F: FnOnce() + Send + 'static,
+    {
+        let job = Box::new(f);
+        self.sender.as_ref().unwrap().send(job).unwrap()
+    }
+}
+
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+        drop(self.sender.take());
+
+        for worker in &mut self.workers {
+            worker.thread.take().unwrap().join().unwrap();
+        }
+    }
+}
+
